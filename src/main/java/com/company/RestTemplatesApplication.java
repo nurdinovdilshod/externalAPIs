cb9714aa23aa83f39713bc8a5fc2b9b93adb163b
Mr_Dilshod <nurdinovdilshod@gmail.com>
2024-06-24 09:43:53 +0500

    Master

---
+++ src/main/java/com/company/RestTemplatesApplication.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestTemplatesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestTemplatesApplication.class, args);
+    }
+
+}
